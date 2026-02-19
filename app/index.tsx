a6820f9cebed023e9ce054dce748ab9faaf16fef
Joshua Couto <joshua.s.couto@gmail.com>
2026-02-19 13:58:08 -0700

    Initial Commit

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+/**
+ *  @author Group 9 - Aaron Reid, Joshua Couto, Kaley Wood
+ *  Southern Alberta Institute of Technology: CPRG-303-C
+ *  Assignment 2: Advanced Multi-Screen Mobile App w/ Collaborative Navigation
+ *  Created: 02.19.2026
+ */
+
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
