9f56faea9692b040775181dd1f90a5617b0cb896
aaron <aareid1999@gmail.com>
2026-03-01 20:31:12 -0700

    added in the quick cards into the home page

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,20 +1,33 @@
+import QuickPickCard from '@/components/home/quickPickCard';
 import TopNavBar from '@/components/home/topNavBar';
+import { quickCardPickData } from '@/data/quickCardpickData';
 import { topNavBarData } from '@/data/topNavBarData';
 import React from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 
 const Home = () => {
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={topNavBarData}
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      showsVerticalScrollIndicator={false}
+    >
+      <ScrollView
         horizontal
         showsHorizontalScrollIndicator={false}
-        keyExtractor={(item) => item.id}
         contentContainerStyle={styles.topNavList}
-        renderItem={({ item }) => <TopNavBar title={item.title} />}
-      />
-    </View>
+      >
+        {topNavBarData.map((item) => (
+          <TopNavBar key={item.id} title={item.title} />
+        ))}
+      </ScrollView>
+
+      <View style={styles.quickPicksGrid}>
+        {quickCardPickData.map((item) => (
+          <QuickPickCard key={item.id} title={item.title} imageSource={item.image} />
+        ))}
+      </View>
+    </ScrollView>
   );
 };
 
@@ -24,10 +37,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#121212',
+  },
+  contentContainer: {
     paddingTop: 56,
+    paddingBottom: 120,
   },
   topNavList: {
     paddingHorizontal: 12,
     gap: 10,
+    marginBottom: 14,
+  },
+  quickPicksGrid: {
+    paddingHorizontal: 12,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
   },
 });
--- data/quickCardpickData.tsx
+++ data/quickCardpickData.tsx
@@ -1,17 +1,17 @@
 import { ImageSourcePropType } from "react-native";
-export interface quickCardPick{
-    id: string;
-    title: string
-    image: ImageSourcePropType;
+export interface QuickCardPick {
+  id: string;
+  title: string;
+  image: ImageSourcePropType;
 }
 
-export const quickCardPickData: quickCardPick[] = [
-    {id:"1", title: "Liked Songs", image:require('"https://misc.scdn.co/liked-songs/liked-songs-640.png" },')},
-    {id:"2", title:"Certified Lover Boy", image:require('https://raw.githubusercontent.com/GymLeaderC/cprg-303-ax02/refs/heads/branch-aaron/assets/images/clb.jpeg')},
-    {id:"3", title:"Deorro", image:require('https://raw.githubusercontent.com/GymLeaderC/cprg-303-ax02/refs/heads/branch-aaron/assets/images/deorro.jpeg')},
-    {id:"4", title:"Drake", image:require('https://raw.githubusercontent.com/GymLeaderC/cprg-303-ax02/refs/heads/branch-aaron/assets/images/Drake.jpeg')},
-    {id:"5", title:"DeBÍ TiRAR MàS FOTos", image:require('https://raw.githubusercontent.com/GymLeaderC/cprg-303-ax02/refs/heads/branch-aaron/assets/images/dtmf.jpeg')},
-    {id:"6", title:"Milo Mae", image:require('https://raw.githubusercontent.com/GymLeaderC/cprg-303-ax02/refs/heads/branch-aaron/assets/images/milomae.jpeg')},
-    {id:"7", title:"Lagaan", image:require('https://raw.githubusercontent.com/GymLeaderC/cprg-303-ax02/refs/heads/branch-aaron/assets/images/Lagaan.jpeg')},
-    {id:"8", title:"Ay Amor Radio", image:require('../assets')},
-]
+export const quickCardPickData: QuickCardPick[] = [
+  { id: "1", title: "Liked Songs", image: require("../assets/images/icon.png") },
+  { id: "2", title: "Certified Lover Boy", image: require("../assets/images/clb.jpeg") },
+  { id: "3", title: "Deorro", image: require("../assets/images/deorro.jpeg") },
+  { id: "4", title: "Drake", image: require("../assets/images/Drake.jpeg") },
+  { id: "5", title: "DeBI TiRAR MaS FOTos", image: require("../assets/images/dtmf.jpeg") },
+  { id: "6", title: "Milo Mae", image: require("../assets/images/milomae.jpeg") },
+  { id: "7", title: "Lagaan", image: require("../assets/images/Lagaan.jpeg") },
+  { id: "8", title: "Ay Amor Radio", image: require("../assets/images/ayamor.jpeg") },
+];
